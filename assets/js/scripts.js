3c30c112f003de0539e6dc0158f7f0069aae7414
Mahmut Namli <mahmudnamli@gmail.com>
2020-07-07 11:15:02 +0300

    fixed wrong removeClass method

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -61,79 +61,79 @@ function getStatsForHome(force) {
 
                 if (!response.results.nginx_installed) { $(".nginx_reload").hide(); $(".nginx_restart").hide(); }
                 if (response.results.nginx_line) { $(".nginx_line").html(response.results.nginx_line); }
-                if (response.results.nginx_status === 1) { $(".nginx_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.nginx_status === 1) { $(".nginx_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-nginx").LoadingOverlay("hide");
 
                 if (!response.results.apache_installed) { $(".apache_restart").hide(); }
                 if (response.results.apache_line) { $(".apache_line").html(response.results.apache_line); }
-                if (response.results.apache_status === 1) { $(".apache_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.apache_status === 1) { $(".apache_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-apache").LoadingOverlay("hide");
 
                 if (!response.results.postfix_installed) { $(".postfix_restart").hide(); }
                 if (response.results.postfix_line) { $(".postfix_line").html(response.results.postfix_line); }
-                if (response.results.postfix_status === 1) { $(".postfix_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.postfix_status === 1) { $(".postfix_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-postfix").LoadingOverlay("hide");
 
                 if (!response.results.dovecot_installed) { $(".dovecot_restart").hide(); }
                 if (response.results.dovecot_line) { $(".dovecot_line").html(response.results.dovecot_line); }
-                if (response.results.dovecot_status === 1) { $(".dovecot_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.dovecot_status === 1) { $(".dovecot_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-dovecot").LoadingOverlay("hide");
 
                 if (!response.results.vsftpd_installed) { $(".vsftpd_restart").hide(); }
                 if (response.results.vsftpd_line) { $(".vsftpd_line").html(response.results.vsftpd_line); }
-                if (response.results.vsftpd_status === 1) { $(".vsftpd_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.vsftpd_status === 1) { $(".vsftpd_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-vsftpd").LoadingOverlay("hide");
 
                 if (!response.results.proftpd_installed) { $(".proftpd_restart").hide(); }
                 if (response.results.proftpd_line) { $(".proftpd_line").html(response.results.proftpd_line); }
-                if (response.results.proftpd_status === 1) { $(".proftpd_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.proftpd_status === 1) { $(".proftpd_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-proftpd").LoadingOverlay("hide");
 
                 if (!response.results.ssh_installed) { $(".ssh_restart").hide(); }
                 if (response.results.ssh_line) { $(".ssh_line").html(response.results.ssh_line); }
-                if (response.results.ssh_status === 1) { $(".ssh_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.ssh_status === 1) { $(".ssh_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-ssh").LoadingOverlay("hide");
 
                 if (!response.results.mysql_installed) { $(".mysql_restart").hide(); }
                 if (response.results.mysql_line) { $(".mysql_line").html(response.results.mysql_line); }
-                if (response.results.mysql_status === 1) { $(".mysql_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.mysql_status === 1) { $(".mysql_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-mysql").LoadingOverlay("hide");
 
                 //
 
                 if (!response.results.php_installed) { $(".php_restart").hide(); }
                 if (response.results.php_line) { $(".php_line").html(response.results.php_line); }
-                if (response.results.php_status === 1) { $(".php_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.php_status === 1) { $(".php_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-php").LoadingOverlay("hide");
 
                 if (!response.results.php56_installed) { $(".php56_restart").hide(); }
                 if (response.results.php56_line) { $(".php56_line").html(response.results.php56_line); }
-                if (response.results.php56_status === 1) { $(".php56_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.php56_status === 1) { $(".php56_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-php56").LoadingOverlay("hide");
 
                 if (!response.results.php70_installed) { $(".php70_restart").hide(); }
                 if (response.results.php70_line) { $(".php70_line").html(response.results.php70_line); }
-                if (response.results.php70_status === 1) { $(".php70_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.php70_status === 1) { $(".php70_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-php70").LoadingOverlay("hide");
 
                 if (!response.results.php71_installed) { $(".php71_restart").hide(); }
                 if (response.results.php71_line) { $(".php71_line").html(response.results.php71_line); }
-                if (response.results.php71_status === 1) { $(".php71_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.php71_status === 1) { $(".php71_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-php71").LoadingOverlay("hide");
 
                 if (!response.results.php72_installed) { $(".php72_restart").hide(); }
                 if (response.results.php72_line) { $(".php72_line").html(response.results.php72_line); }
-                if (response.results.php72_status === 1) { $(".php72_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.php72_status === 1) { $(".php72_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-php72").LoadingOverlay("hide");
 
                 if (!response.results.php73_installed) { $(".php73_restart").hide(); }
                 if (response.results.php73_line) { $(".php73_line").html(response.results.php73_line); }
-                if (response.results.php73_status === 1) { $(".php73_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.php73_status === 1) { $(".php73_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-php73").LoadingOverlay("hide");
 
                 if (!response.results.php74_installed) { $(".php74_restart").hide(); }
                 if (response.results.php74_line) { $(".php74_line").html(response.results.php74_line); }
-                if (response.results.php74_status === 1) { $(".php74_status").removeClass("passive-server").removeClass("active-server"); }
+                if (response.results.php74_status === 1) { $(".php74_status").removeClass("passive-server").addClass("active-server"); }
                 $("#status-php74").LoadingOverlay("hide");
             }
         });
